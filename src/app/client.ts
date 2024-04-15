8e7aa9d0f0c1afb395e4f593818e3489937c3c9f
davidmansion <93778907+davidmansion@users.noreply.github.com>
2024-04-15 07:04:24 +0100

    Update client.ts

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,7 +2,7 @@ import { createThirdwebClient } from 'thirdweb';
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = 8528fd0a51902af98d29e1bf3cef6192 ;
+const clientId = "8528fd0a51902af98d29e1bf3cef6192" ;
 
 if (!clientId) {
 	throw new Error('No client ID provided');
